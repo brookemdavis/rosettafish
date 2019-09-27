469676179a5f0030603791899270f4bc20afef13
Brooke Davis <brooke.davis@dfo-mpo.gc.ca>
2019-09-27 10:18:08 -0700

    terms updated up to figure 6

--- data-raw/make-dictionary.R
+++ data-raw/make-dictionary.R
@@ -1,4 +1,11 @@
+library(dplyr)
 rosetta_terms <- as.data.frame(readr::read_csv("inst/extdata/terms.csv"))
+
+# # read in my terms
+# new_terms <- read.csv("R:/SCIENCE/Chinook_QAMS_program/5Yr_Review/5Year_Review_Revisions_InProgress/French Fig Labels.csv", encoding = "UTF-8")
+# names(new_terms)[1] <- "english"
+#
+# rosetta_terms <- full_join(rosetta_terms, new_terms)
 
 language <- "english"
 rosetta_terms <- rosetta_terms[order(rosetta_terms[[language]]), , drop = FALSE]
@@ -18,4 +25,6 @@ if(length(dups>0)){
 }
 
 usethis::use_data(rosetta_terms, overwrite = TRUE, internal = TRUE)
+
+devtools::install(quick = TRUE, dependencies = FALSE)
 
